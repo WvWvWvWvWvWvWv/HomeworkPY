0acdabc30667d450bb3edb7613d4a5903acb7dc9
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-06-12 08:15:15 +0300

    Homework 6

---
+++ task32.py
@@ -0,0 +1,6 @@
+list_1 = [int(i) for i in input().split()]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
